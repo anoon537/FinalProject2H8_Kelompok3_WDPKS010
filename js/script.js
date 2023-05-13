3cea9f514e298934f9923fff3631ce01bd4323bc
Anoon <muhammadmp442@gmail.com>
2023-05-13 10:09:28 +0700

    Remove Scrolling Effect & Add Jumbotron

--- js/script.js
+++ js/script.js
@@ -31,27 +31,6 @@ window.onload = function () {
   }
 };
 
-// efek fade in/out ketika scroll ke setiap section
-const sections = document.querySelectorAll("section");
-
-sections.forEach((section) => {
-  const sectionTop = section.offsetTop;
-  const sectionHeight = section.clientHeight;
-  window.addEventListener("scroll", function () {
-    const scrollY = window.pageYOffset;
-    const scrollBottom = scrollY + window.innerHeight;
-    if (
-      scrollBottom > sectionTop + sectionHeight / 2 &&
-      scrollY < sectionTop + sectionHeight
-    ) {
-      section.style.opacity = 1;
-      section.style.transition = "opacity 1s ease-in-out";
-    } else {
-      section.style.opacity = 0;
-    }
-  });
-});
-
 // tombol back to top
 const backToTopButton = document.querySelector(".back-to-top");
 
